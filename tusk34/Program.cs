de992fa2ead10d1cd92d5c5671dc65643d029b02
Evgeni.ok14 <evgeni.ok@mail.ru>
2022-12-15 10:01:06 +0300

    added a new project: tusk34

---
+++ tusk34/Program.cs
@@ -0,0 +1,35 @@
+﻿void InputArray(int[] array)
+{
+    for (int i=0; i < array.Length; i++)
+     array[i] = new Random().Next(99,999);
+}
+void PrintArray(int[]array) 
+{
+    Console.Write($"[{String.Join(", ", array)}]");
+}
+     
+         int ReleaseArray(int[] array, int n)
+        { 
+      
+             int SumIndex = 0;
+        
+                 for (int i = 0; i < array.Length; i++)
+               {
+               
+                  if (array[i] % 2 == 0)
+               
+                    SumIndex = SumIndex + 1;
+               }
+    
+            return SumIndex;
+      }
+
+ Console.Clear();
+ Console.Write("Введите число элементов массива: ");
+ int n = Convert.ToInt32(Console.ReadLine());
+ int [] array = new int [n];
+ InputArray(array);
+ PrintArray(array);
+ Console.WriteLine();
+ ReleaseArray(array, n);
+ Console.WriteLine("Количество нечётных элементов равно: " + ReleaseArray(array, n));
